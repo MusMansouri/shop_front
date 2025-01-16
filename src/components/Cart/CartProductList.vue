20af468ec737ac7382cd04188ee0180dd4dc4728
Mansouri Mustapha <mansouri.mustapha.a@gmail.com>
2025-01-16 14:07:58 +0100

    first commit

---
+++ src/components/Cart/CartProductList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import CartProduct from "./CartProduct.vue";
+
+defineProps({
+  cartlist: Array,
+});
+</script>
+
+<template>
+  <div>
+    <CartProduct :product="product" v-for="product in cartlist" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
